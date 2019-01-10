3c2932559b9e7214912aa0b39f7d38f28ed1188d
chujunjie <c.junjie@hotmail.com>
2019-01-11 00:16:48 +0800

    单例的破坏和解决办法

--- src/main/java/com/demo/designpatterns/singletonPattern/HungrySingleton.java
+++ src/main/java/com/demo/designpatterns/singletonPattern/HungrySingleton.java
@@ -1,12 +1,14 @@
 package com.demo.designpatterns.singletonPattern;
 
+import java.io.Serializable;
+
 /**
  * @Description: 饿汉式：类被加载时创建，没有懒加载的效果，降低了内存利用率
  * @Author: chujunjie
  * @Date: Create in 12:36 2018/8/5
  * @Modified By
  */
-public class HungrySingleton {
+public class HungrySingleton implements Serializable {
 
     private static HungrySingleton instance = new HungrySingleton();
 
@@ -14,6 +16,11 @@ public class HungrySingleton {
     }
 
     public static HungrySingleton getInstance() {
+        return instance;
+    }
+
+    // 注意不能是static的readResolve，否则获取不到
+    private Object readResolve() {
         return instance;
     }
 }
--- src/main/java/com/demo/designpatterns/singletonPattern/SingletonPatternDemo.java
+++ src/main/java/com/demo/designpatterns/singletonPattern/SingletonPatternDemo.java
@@ -1,5 +1,7 @@
 package com.demo.designpatterns.singletonPattern;
 
+import java.io.*;
+
 /**
  * @Description: 单例模式：确保一个类只产生一个实例，并提供一个全局访问点。
  *
@@ -9,11 +11,24 @@ package com.demo.designpatterns.singletonPattern;
  * @Modified By
  */
 public class SingletonPatternDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         HungrySingleton hungrySingleton = HungrySingleton.getInstance();
         LazySingleton lazySingleton = LazySingleton.getInstance();
         DoubleCheckSingleton doubleCheckSingleton = DoubleCheckSingleton.getInstance();
 
+        // 序列化对单例的破坏
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singleton_file"));
+        oos.writeObject(hungrySingleton);
+        File file = new File("singleton_file");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+
+        /* readObject方法读取可序列化的类会通过反射new一个instance
+         * 并且其中hasReadResolveMethod方法会判断，如果读取的是一个有readResolve
+         * 方法的序列化类则会通过反射再把readResolve返回的值赋值回去 */
+        HungrySingleton newSingleton = (HungrySingleton) ois.readObject();
+
+        System.out.println(hungrySingleton);
+        System.out.println(newSingleton);
     }
 
 }
