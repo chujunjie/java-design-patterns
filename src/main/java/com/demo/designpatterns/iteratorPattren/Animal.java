19e69a67e2e87aefca9619ea89002c2f84e25bd6
chujunjie <c.junjie@hotmail.com>
2019-01-27 23:04:14 +0800

    迭代器模式

---
+++ src/main/java/com/demo/designpatterns/iteratorPattren/Animal.java
@@ -0,0 +1,19 @@
+package com.demo.designpatterns.iteratorPattren;
+
+/**
+ * @Description:
+ * @Author: chujunjie
+ * @Date: Create in 22:36 2019/1/27
+ * @Modified By
+ */
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
